1d4eb4df20766a18eea1e146dabaec69e0ee2001
Shanawaz Islam <sis@nisportal.com>
2018-11-08 11:25:47 +0100

    Initial commit

---
+++ ThreadSafety/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ThreadSafety
+{
+    public class Account
+    {
+        private readonly object _balanceLock = new object();
+        private decimal _balance;
+
+        public Account(decimal initialBalance)
+        {
+            _balance = initialBalance;
+        }
+
+        public decimal Debit(decimal amount)
+        {
+            lock (_balanceLock)
+            {
+                if (_balance < amount) return 0;
+
+                Console.WriteLine($"Balance before debit :{_balance,5}");
+                Console.WriteLine($"Amount to remove     :{amount,5}");
+                _balance = _balance - amount;
+                Console.WriteLine($"Balance after debit  :{_balance,5}");
+                Console.WriteLine("____________________________________");
+
+                return amount;
+            }
+        }
+
+        public void Credit(decimal amount)
+        {
+            lock (_balanceLock)
+            {
+                Console.WriteLine($"Balance before credit:{_balance,5}");
+                Console.WriteLine($"Amount to add        :{amount,5}");
+                _balance = _balance + amount;
+                Console.WriteLine($"Balance after credit :{_balance,5}");
+                Console.WriteLine("___________________________________");
+            }
+        }
+    }
+}
---
+++ ThreadSafety/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ThreadSafety
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/lock-statement
+
+            var account = new Account(1000);
+            var tasks = new Task[10];
+            for (var i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = Task.Run(() => RandomlyUpdate(account));
+            }
+            Task.WaitAll(tasks);
+
+            Console.ReadKey();
+        }
+
+        static void RandomlyUpdate(Account account)
+        {
+            var rnd = new Random();
+            for (var i = 0; i < 10; i++)
+            {
+                var amount = rnd.Next(1, 100);
+                var doCredit = rnd.NextDouble() < 0.5;
+                if (doCredit)
+                {
+                    account.Credit(amount);
+                }
+                else
+                {
+                    account.Debit(amount);
+                }
+            }
+        }
+    }
+}
