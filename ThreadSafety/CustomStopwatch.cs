bf6912f709c4b4cd38938a34f52414423873bafa
Shanawaz Islam <sis@nisportal.com>
2019-01-11 15:42:14 +0100

    Checked max degree of parallelism

---
+++ ThreadSafety/CustomStopwatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ThreadSafety
+{
+    //https://stackoverflow.com/a/23948372
+    public class CustomStopwatch : Stopwatch
+    {
+        public DateTime? StartAt { get; private set; }
+        public DateTime? EndAt { get; private set; }
+
+        public void Start()
+        {
+            StartAt = DateTime.Now;
+            base.Start();
+        }
+
+        public void Stop()
+        {
+            EndAt = DateTime.Now;
+            base.Stop();
+        }
+
+        public void Reset()
+        {
+            StartAt = null;
+            EndAt = null;
+            base.Reset();
+        }
+
+        public void Restart()
+        {
+            StartAt = DateTime.Now;
+            EndAt = null;
+            base.Restart();
+        }
+    }
+}
--- ThreadSafety/Program.cs
+++ ThreadSafety/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ThreadSafety
@@ -11,20 +12,63 @@ namespace ThreadSafety
 
             var account = new Account(1000);
             var tasks = new Task[10];
-            
+
             //for (var i = 0; i < tasks.Length; i++)
             //{
             //    tasks[i] = Task.Run(() => RandomlyUpdate(account));
             //}
 
-            Parallel.For(0, tasks.Length, i =>
-            {
-                tasks[i] = Task.Run(() => RandomlyUpdate(account));
-            });
+            //Parallel.For(0, tasks.Length, i =>
+            //{
+            //    tasks[i] = Task.Run(() => RandomlyUpdate(account));
+            //});
 
-            Task.WaitAll(tasks);
+            //Task.WaitAll(tasks);
+
+            Parallel.ForEach(
+                tasks, 
+                new ParallelOptions
+                {
+                    MaxDegreeOfParallelism = 3
+                }, 
+                task =>
+                {
+                    var stopwatch = new CustomStopwatch();
+                    stopwatch.Start();
+
+                    RandomlyUpdate(account);
+
+                    stopwatch.Stop();
+
+                    if (stopwatch.StartAt.HasValue && stopwatch.EndAt.HasValue)
+                    {
+                        Console.WriteLine("Stopwatch elapsed: {0}, Start at: {1}, End at: {2}", stopwatch.ElapsedMilliseconds, stopwatch.StartAt.Value, stopwatch.EndAt.Value);
+                    }
+                    else
+                    {   Console.WriteLine("Stopwatch elapsed: {0}", stopwatch.ElapsedMilliseconds);
+                    }
+                });
 
             Console.ReadKey();
+        }
+
+        public static async Task RunLimitedNumberAtATime<T>(int numberOfTasksConcurrent, IEnumerable<T> inputList, Func<T, Task> asyncFunc)
+        {
+            var inputQueue = new Queue<T>(inputList);
+            var runningTasks = new List<Task>(numberOfTasksConcurrent);
+            for (var i = 0; i < numberOfTasksConcurrent && inputQueue.Count > 0; i++)
+            {
+                runningTasks.Add(asyncFunc(inputQueue.Dequeue()));
+            }
+
+            while (inputQueue.Count > 0)
+            {
+                Task task = await Task.WhenAny(runningTasks);
+                runningTasks.Remove(task);
+                runningTasks.Add(asyncFunc(inputQueue.Dequeue()));
+            }
+
+            await Task.WhenAll(runningTasks);
         }
 
         static void RandomlyUpdate(Account account)
